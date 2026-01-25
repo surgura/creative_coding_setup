385d268d81a3e19bd97be401a8af8ed86bc501b1
SteamOS User <aartstuurman@hotmail.com>
2026-01-25 17:25:47 +0100

    sdf

--- hydra/main5.js
+++ hydra/main5.js
@@ -1,6 +1,7 @@
 await loadScript("scripts/plugins/hydra-src.js")
 await loadScript("scripts/plugins/hydra-text.js")
 await loadScript("scripts/plugins/hydra-mouse.js")
+await loadScript("scripts/plugins/hydra-glsl.js")
 await loadScript("scripts/windows.js")
 
 hydraText.font = "interceptor"
@@ -10,20 +11,30 @@ s0.initCam(0)
 s1.initCam(1)
 s2.initCam(2)
 
-// voronoi()
-// 	.kaleid(4)
-
 function winRotate(n) {
-    return Math.sin(time*(1+n/10)+n)/30;
+    return Math.sin(time * (1 + n / 10) + n) / 30;
 }
 
-solid(0.1,0.1,0.1)
-.layer(srcRelMask(s0)
-.toWindow(1, () => winRotate(1)))
-.layer(srcRelMask(s0)
-.toWindow(2, () => winRotate(2)))
-.layer(voronoi()
+srcRelMask(s0)
+.out(o1)
+srcRelMask(s0)
+.modulate(voronoi())
+.out(o2)
+
+// voronoi()
+// solid(0,0,0)
+glsl('vec2 p=st-0.5; float d=length(p)-0.25; vec4(vec3(1.0-smoothstep(0.0,0.003,d)),1.0)')
 .noWrap()
+.out(o3)
+
+background = solid(0.2, 0.2, 0.2)
+
+background
+.layer(srcMask(o1)
+.toWindow(1, () => winRotate(1)))
+.layer(srcMask(o2)
+.toWindow(2, () => winRotate(2)))
+.layer(srcMask(o3)
 .toWindow(3, () => winRotate(3)))
 
 .layer(text("Neon Move")
@@ -33,4 +44,4 @@ solid(0.1,0.1,0.1)
 .modulate(voronoi())
 .modulate(osc(1, 1, 1))
 )
-.out()
+.out(o0)
